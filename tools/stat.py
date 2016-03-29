ee5248014c2bacbcaebfc172e425239044ddb2e1
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-29 16:51:17 +0900

    箱ひげ図とか作った

--- tools/stat.py
+++ tools/stat.py
@@ -23,32 +23,36 @@ for _type, varis in o.items():
         s = set()
         print(_type
             ,"\t",key
-            ,"\t",len(arr)
+        #    ,"\t",len(arr)
         )
         for color1, v1 in mtx.items():
             s.add(color1)
             for color2, v2 in v1.items():
                 if color2 in s: continue
-                if color1 == "cyan" or color1 == "lime": color1 += "___"
-                if color2 == "cyan" or color2 == "lime": color2 += "___"
-                if color1 == "yellow": color1 += "_"
-                if color2 == "yellow": color2 += "_"
+                #if color1 == "cyan" or color1 == "lime": color1 += "___"
+                #if color2 == "cyan" or color2 == "lime": color2 += "___"
+                #if color1 == "yellow": color1 += "_"
+                #if color2 == "yellow": color2 += "_"
                 a = np.array(v2)
                 print(
+                    #_type
+                    #+"-"+key
+                    #+"-"+str(len(arr))
+                    #+"-"+
                     str(len(v2))
                     ,"\t",color1
-                    ,"\t",color2
-                    ,"\t",np.average(a)  ## 平均
-                    ,"\t",np.mean(a)       ## 算術平均
-                    ,"\t",np.median(a)     ## 中央値
-                    ,"\t",stats.scoreatpercentile(a, 25) #第2四分位
-                    ,"\t",stats.scoreatpercentile(a, 50) #中央値
-                    ,"\t",stats.scoreatpercentile(a, 75) #第3四分位
-                    ,"\t",np.ptp(a)        ## 値の範囲(最大値-最小値)
-                    ,"\t",np.amax(a)       ## 最大値
+                    +"-"+color2
+                    #,"\t",np.average(a)  ## 平均
+                    #,"\t",np.mean(a)       ## 算術平均
+                    #,"\t",np.median(a)     ## 中央値
                     ,"\t",np.amin(a)       ## 最小値
-                    ,"\t",np.var(a)        ## 分散
-                    ,"\t",np.std(a)        ## 標準偏差
+                    ,"\t",stats.scoreatpercentile(a, 25) #第2四分位
+                    #,"\t",stats.scoreatpercentile(a, 50) #中央値
+                    ,"\t",stats.scoreatpercentile(a, 75) #第3四分位
+                    ,"\t",np.amax(a)       ## 最大値
+                    #,"\t",np.ptp(a)        ## 値の範囲(最大値-最小値)
+                    #,"\t",np.var(a)        ## 分散
+                    #,"\t",np.std(a)        ## 標準偏差
                 )
 
 
